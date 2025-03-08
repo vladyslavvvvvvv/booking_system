b4721870fed3272fd087cc72c1d4a549fb96cf7e
vladyslavvvvvvv <zebrochka228333@gmail.com>
2025-03-08 19:56:46 +0100

    added better frontend to my project

--- booking/urls.py
+++ booking/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
-from views import rooms_list
-
-path("rooms_list", rooms_list)
+from .views import apartment_list, create_apartment
+urlpatterns = [
+    path("apartment/list/", apartment_list, name="apartment-list"),
+    path("apartment/create/", create_apartment, name="create-apartment" )
+]
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .models import Apartment
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def apartment_list(request: HttpRequest):
+    apartments_list = Apartment.objects.all()
+    context = {
+        "apartments_list": apartments_list
+    }
+    return render(request, "booking/apartment_list.html", context= context)
+
+def create_apartment(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, "booking/create_apartment.html")
+    if request.method == "POST":
+        apartment_location = request.POST.get("location")
+        apartment_price = request.POST.get("price")
+        apartment_available = request.POST.get("available", False)
+
+        return redirect(reverse("apartments-list"))
